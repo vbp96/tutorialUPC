c36965452952b00859b0be1952f4dce183eccbed
bhoscar <bhoscar@hotmail.com>
2016-08-17 07:54:57 -0500

    Primera Versión

---
+++ appSesion2POO/src/main/java/pe/edu/upc/appSesion2POO/App.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.appSesion2POO;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
